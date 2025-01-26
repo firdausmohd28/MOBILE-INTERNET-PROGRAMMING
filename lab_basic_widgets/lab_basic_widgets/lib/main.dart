719da60cfdb919d68e090232092f6c85fbbe1308
firdausmohd28 <nur_firdaus@soc.uum.edu.my>
2025-01-27 00:30:28 +0800

    Update main.dart

--- lab_basic_widgets/lab_basic_widgets/lib/main.dart
+++ lab_basic_widgets/lab_basic_widgets/lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 
-//Step 2: Application structure
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
+//Step 2: Application Structure
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,58 +16,84 @@ class MyApp extends StatelessWidget {
 }
 
 class BasicWidgetsDemo extends StatefulWidget {
-  const BasicWidgetsDemo({super.key});
-
   @override
-  // ignore: library_private_types_in_public_api
   _BasicWidgetsDemoState createState() => _BasicWidgetsDemoState();
 }
 
 class _BasicWidgetsDemoState extends State<BasicWidgetsDemo> {
-  // ignore: prefer_final_fields
   TextEditingController _controller = TextEditingController();
 
   @override
-  Widget build(BuildContext contex) {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.pink,
       ),
 
-      //Step 3: Implement Widgets
-      body: Center(
+      //Step 3: Implement Widget
+      //Step 5: Enhancing the application
+
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
         child: Column(children: [
           Text(
             'Welcome to Flutter Basics',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            style: TextStyle(
+                fontSize: 24, fontWeight: FontWeight.bold, color: Colors.pink),
           ),
-
-          TextField(
-            controller: _controller,
-            decoration: InputDecoration(
-              labelText: 'Enter your name',
-              border: OutlineInputBorder(),
+          Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Enter Your Name',
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(-200)),
+              ),
             ),
           ),
           ElevatedButton(
             onPressed: () {
+              button1();
               setState(() {
-                //Update UI
+                //update UI
               });
             },
-            child: Text('Submit'),
+            child: Text('Submit',
+                style: TextStyle(fontSize: 15, color: Colors.pink)),
           ),
-          //Column Widget
           Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Text('Welcome to Flutter Basic'),
+                SizedBox(height: 20),
+                Text(
+                  'Welcome to Flutter Basics',
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.pink),
+                ),
                 TextField(controller: _controller),
-                ElevatedButton(onPressed: () {}, child: Text('Submit')),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    button2();
+                  },
+                  child: Text('Submit',
+                      style: TextStyle(fontSize: 15, color: Colors.pink)),
+                ),
               ]),
         ]),
       ),
     );
+  }
+
+  void button1() {
+    print('Welcome!');
+  }
+
+  button2() {
+    print(_controller.text);
   }
 }
